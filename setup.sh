0a62d038ef9223d1a58903c0a9e14f354263f365
Tino Rusch <tino.rusch@webvariants.de>
2016-04-14 11:17:47 +0200

    added distinction between Ubuntu and debian;

--- setup.sh
+++ setup.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-sudo apt-get --yes install golang gcc systemd-container rng-tools
+if uname -a | grep Ubuntu; then
+  sudo apt-get --yes install golang gcc systemd-container rng-tools
+fi
 
-# this code is for debian stable only. TODO: wrap this in "if on debian"
-expected=3.18
-received=$(uname -r)
-min=$(echo -e $expected"\n"$received|sort -V|head -n 1)
-if ! [ "$min" = "$expected" ];then
-  sudo bash -c 'echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list'
-  sudo apt-get update
-  sudo apt-get --yes -t jessie-backports install linux-image-amd64
-  echo "We needed to update your kernel. Please reboot and run this script again."
-  exit 0
+if uname -a | grep Debian; then
+  sudo apt-get --yes install golang gcc rng-tools
+  expected=3.18
+  received=$(uname -r)
+  min=$(echo -e $expected"\n"$received|sort -V|head -n 1)
+  if ! [ "$min" = "$expected" ];then
+    sudo bash -c 'echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list'
+    sudo apt-get update
+    sudo apt-get --yes -t jessie-backports install linux-image-amd64
+    echo "We needed to update your kernel. Please reboot and run this script again."
+    exit 0
+  fi
 fi
 
 # setup go
